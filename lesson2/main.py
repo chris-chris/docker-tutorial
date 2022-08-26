03623552866dde4e5279c0be308ecee842df48f4
송호연 <sjhshy@gmail.com>
2022-08-26 11:13:43 +0900

    update

---
+++ lesson2/main.py
@@ -0,0 +1,15 @@
+import os
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def hello_world():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ["PORT"]))
