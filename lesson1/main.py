8f718174eadb16759426c3ead18ca4b5cc89e2d0
송호연 <sjhshy@gmail.com>
2022-08-26 11:45:23 +0900

    publish docker image

--- lesson1/main.py
+++ lesson1/main.py
@@ -8,7 +8,7 @@ def hello_world():
     return {"message": "Hello World"}
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
